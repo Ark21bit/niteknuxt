bcfb1775053b15bd52f80f430f1ebb5f42cdd934
ark21yan <120375741+ark21yan@users.noreply.github.com>
2023-01-30 11:44:56 +0000

    robots.txt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules:[    
-    ['nuxt-swiper',{ styleLang: 'css', modules: ['navigation', 'thumbs', 'pagination'],}],            
+    ['nuxt-swiper',{ styleLang: 'css', modules: ['navigation', 'thumbs', 'pagination'],}], 
+    ['@nuxtjs/robots', {configPath:"~/robots.config.js" }], 
+  /*   ['nuxt-simple-sitemap',{hostname:'https://ark21yan-automatic-spork-664p4j64vqxc5pg6-3001.preview.app.github.dev'}] */
   ], 
+  
+ 
     css: [
         '~/assets/css/fonts.css',        
         '~/assets/css/main.css',        
